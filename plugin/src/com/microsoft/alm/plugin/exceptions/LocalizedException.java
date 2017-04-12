490f76cc9b9e07f228a75de4c8bf0744098f3d6c
Yang Cao (VSNC) <yacao@microsoft.com>
2017-04-12 13:59:44 -0400

    remove almost all

--- plugin/src/com/microsoft/alm/plugin/exceptions/LocalizedException.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root.
-
-package com.microsoft.alm.plugin.exceptions;
-
-public interface LocalizedException {
-    String getMessageKey();
-    String[] getMessageParameters();
-}
